c9bdc5295240d440aebe09417048e670380b958c
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-27 18:06:25 -0600

    Se crea el paquete modelo con su clase producto

--- src/main/java/org/example/Model/ProductoModel.java
+++ src/main/java/org/example/Model/ProductoModel.java
@@ -1,4 +1,13 @@
 package org.example.Model;
 
 public class ProductoModel {
+     public String nombre;
+  private   int  cantidad;
+    String categoria;
+    double precio;
+    double costo;
+    Boolean Reservado;
+
+
+
 }
