4738527e797f3d3648923f6358548ef3ce1f36bc
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-27 18:11:55 -0600

    Se declara el objeto producto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
 
+import org.example.Model.ProductoModel;
+
 public class Main {
     public static void main(String[] args) {
+        ProductoModel producto;
+        
 
 
     }
