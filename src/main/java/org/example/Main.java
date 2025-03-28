9546d65b24fd167be58c59dfe680ff58af14cce6
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-27 18:56:33 -0600

    Se crea los accesores y modificadores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,14 @@ import org.example.Model.ProductoModel;
 
 public class Main {
     public static void main(String[] args) {
-       ProductoModel producto = new ProductoModel("Audifonos",12,"Electronicos",78.78,100,false);
+       ProductoModel producto = new ProductoModel();
+       producto.setNombre("Audifonos");
+       producto.setCantidad(12);
+       producto.setCosto(78.8);
+
+       System.out.println(producto.getNombre());
+       System.out.println(producto.getCantidad());
+       System.out.println(producto.getcosto());
 
 
 
--- src/main/java/org/example/Model/ProductoModel.java
+++ src/main/java/org/example/Model/ProductoModel.java
@@ -25,4 +25,49 @@ public class ProductoModel {
         this.categoria = categoria;
     }
 
+    // accesores permitir leer valor
+
+    public String getNombre(){
+        return  this.nombre;
+    }
+
+    public int getCantidad(){
+         return this.cantidad;
+    }
+
+    public String getCategoria(){
+        return this.categoria;
+    }
+
+    public  Double getPrecio(){
+
+        return this.precio;
+    }
+
+    public Double getcosto(){
+
+        return this.costo;
+    }
+
+
+
+    // Modificadores Seterar la propiedad
+
+    public void  setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void  setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+
+    public  void  setCosto(double costo){
+        this.costo = costo;
+    }
+
+
+
+
+
+
 }
